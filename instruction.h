54f237e3aaa931178ab0d2a91ab736769fb11428
AarnoT <AarnoT@users.noreply.github.com>
2017-12-11 09:31:15 +0200

    Add struct for instructions

---
+++ instruction.h
@@ -0,0 +1,21 @@
+#include <cstdint>
+#include <string>
+#include <vector>
+
+enum FlagEffect {
+    APPLY = 0,
+    IGNORE,
+    SET,
+    CLEAR
+};
+
+typedef struct Instruction {
+    std::string name;
+    std::uint32_t bytes;
+    std::uint32_t operand_count;
+    std::string operand1;
+    std::string operand2;
+    std::uint32_t cycles;
+    std::uint32_t branch_cycles;
+    std::vector<FlagEffect> flags_ZNHC;
+} Instruction;
