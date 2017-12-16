0900d63e56823eebbdb458322353f9f46feb2484
AarnoT <AarnoT@users.noreply.github.com>
2017-12-16 20:19:20 +0200

    Create SDL window

--- emulator.cpp
+++ emulator.cpp
@@ -23,14 +23,24 @@ bool quit = false;
 
 int main(int argc, char* argv[])
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING & ~SDL_INIT_VIDEO) != 0) {
-	cout << "SDL2 failed to initialize: " << SDL_GetError() << "\n";
-    }
-
     if (argc < 2) {
 	cout << "Enter ROM filename as an argument.\n";
 	return 0;
     }
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+	cout << "SDL2 failed to initialize: " << SDL_GetError() << "\n";
+	return 0;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("GameBoy Emulator",
+		                          SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+					  SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+	cout << "Failed to create window: " << SDL_GetError() << "\n";
+	return 0;
+    }
+    SDL_Surface* display_surface = SDL_GetWindowSurface(window);
 
     const uint32_t buf_size = 0x60000;
     uint8_t rom_buffer[buf_size];
@@ -46,12 +56,17 @@ int main(int argc, char* argv[])
 	last_time_ms = current_time_ms;
 
 	while (cycles_to_catch_up > 0) {
+	    break;
 	    uint8_t cycles_executed = execute_op(state) / 4;
 	    if (cycles_executed > cycles_to_catch_up + 20) {break;}
 	    cycles_to_catch_up -= cycles_executed;
 	}
+
+	handle_events();
+	SDL_UpdateWindowSurface(window);
     }
 
+    SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
@@ -61,4 +76,13 @@ void load_rom(string filename, uint8_t* rom_buffer)
     copy(istreambuf_iterator<char>(rom_file),
          istreambuf_iterator<char>(),
 	 rom_buffer);
+}
+
+void handle_events() {
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+        if (e.type == SDL_QUIT) {
+	    quit = true;
+	}
+    }
 }
--- emulator.h
+++ emulator.h
@@ -4,5 +4,9 @@
 
 extern bool quit;
 
+const int SCREEN_WIDTH = 256;
+const int SCREEN_HEIGHT = 256;
+
 int main(int argc, char* argv[]);
 void load_rom(std::string filename, std::uint8_t* rom_buffer);
+void handle_events();
