5720cdfd0a3ea59321fd2390c9ea83e43c566b81
AarnoT <AarnoT@users.noreply.github.com>
2018-01-04 06:16:50 +0200

    Initialize memory to 0

--- state.cpp
+++ state.cpp
@@ -12,16 +12,18 @@ using std::uint8_t;
 using std::uint16_t;
 using std::uint32_t;
 
-State::State(uint8_t* buffer, uint32_t size) : memory(new uint8_t[0x60000])
+State::State(uint8_t* buffer, uint32_t size) : memory(new uint8_t[0x60000]{0})
 {
     copy(buffer, buffer + size, this->memory);
 }
 
-State::~State() {
+State::~State()
+{
     delete this->memory;
 }
 
-uint8_t State::read_memory(uint16_t addr) {
+uint8_t State::read_memory(uint16_t addr)
+{
     if (true) {
 	return this->memory[addr];
     } else {
@@ -30,7 +32,8 @@ uint8_t State::read_memory(uint16_t addr) {
     return 0;
 }
 
-void State::write_memory(uint16_t addr, uint8_t value) {
+void State::write_memory(uint16_t addr, uint8_t value)
+{
     if (addr >= 0x8000 && addr < 0xffff) {
         this->memory[addr] = value;
     } else {
