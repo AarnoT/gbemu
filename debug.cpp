a13c4641e048f243e571cb327e36fc117ff16891
AarnoT <AarnoT@users.noreply.github.com>
2018-01-10 07:09:08 +0200

    Add deque to track recent jump targets

--- debug.cpp
+++ debug.cpp
@@ -2,16 +2,35 @@
 #include "instruction.h"
 #include "state.h"
 
+#include <algorithm>
 #include <cstdint>
+#include <deque>
 #include <iostream>
 #include <vector>
 
 using std::cout;
+using std::deque;
+using std::find;
 using std::hex;
 using std::uint8_t;
 using std::uint16_t;
 using std::vector;
  
+deque<uint16_t> recent_jumps;
+
+void add_jump(std::uint16_t addr)
+{
+    auto jump = find(recent_jumps.begin(), recent_jumps.end(), addr);
+    if (jump != recent_jumps.end()) {
+        recent_jumps.erase(jump);
+    }
+
+    recent_jumps.push_front(addr);
+    if (recent_jumps.size() > 10) {
+        recent_jumps.pop_back();
+    }
+}
+
 void print_debug_info(State& state, Instruction& instruction, vector<uint8_t> op_code)
 {
     cout << "Op code: ";
@@ -27,8 +46,8 @@ void print_debug_info(State& state, Instruction& instruction, vector<uint8_t> op
     cout << "DE: " << hex << ((state.d << 8) | state.e) << " ";
     cout << "HL: " << hex << ((state.h << 8) | state.l) << "\n";
     cout << "A: " << hex << (uint16_t) state.a << " ";
-    cout << "Flags: " << ((state.f & 0x80) != 0) << ((state.f & 0x40) != 0) <<
-	                 ((state.f & 0x20) != 0) << ((state.f & 0x10) != 0) <<  "\n";
+    cout << "Flags: " << ((state.f & 0x80) != 0) << ((state.f & 0x40) != 0)
+	              << ((state.f & 0x20) != 0) << ((state.f & 0x10) != 0) <<  "\n";
     cout << hex << "LCDC: " << (uint16_t) state.read_memory(0xff40) << " ";
     cout << hex << "STAT: " << (uint16_t) state.read_memory(0xff41) << " ";
     cout << hex << "LY: " << (uint16_t) state.read_memory(0xff44) << "\n\n";
--- debug.h
+++ debug.h
@@ -1,7 +1,14 @@
+#pragma once
+
 #include "instruction.h"
 #include "state.h"
 
 #include <cstdint>
+#include <deque>
 #include <vector>
+
+extern std::deque<std::uint16_t> recent_jumps;
+
+void add_jump(std::uint16_t addr);
 
 void print_debug_info(State& state, Instruction& instruction, std::vector<std::uint8_t> op_code);
